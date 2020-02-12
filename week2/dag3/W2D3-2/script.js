33b4f2853bb18e07aa9a61e3fc8a124bfabbe34e
Kim Litjens <Kimlitjens4@gmail.com>
2020-02-12 11:25:45 +0100

    add W2D3-2

---
+++ week2/dag3/W2D3-2/script.js
@@ -0,0 +1,62 @@
+/*
+const arrayWithWords = ["nice","awesome","tof"]
+const addTheWordCool = function(array){
+    array.push("cool");
+    return array;
+}
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+
+const amountOfElementsInArray = ['appels', 'peren', 'citroenen'];
+console.log(amountOfElementsInArray.length);
+
+const amountOfElementsInArray = function(array) {
+    return array.length;
+}
+console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen']));
+
+
+const selectBelgiumFromBenelux = function(array) {
+    return array[0];
+}
+console.log(selectBelgiumFromBenelux(["Belgie", "Nederland", "Luxemburg"]));
+
+
+
+
+const lastElementInArray = function(array) {
+    return array[array.length - 1];
+  };
+
+console.log(lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"]));
+
+
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+
+const impeachTrumpSlice = function(array) {
+    return array.slice(1, )
+}
+const impeachTrumpSplice = function(array) {
+     array.splice(0, 1)
+     return array
+}
+
+console.log(impeachTrumpSlice(presidents)); 
+console.log(presidents)
+console.log(impeachTrumpSplice(presidents)); 
+console.log(presidents);
+
+
+const stringsTogether = function(array) {
+    return array.join(" ")
+}
+console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}']));
+
+const array1 = [1,2,3];
+const array2 = [4,5,6];
+
+const combineArrays = function(a, b){
+    return a.concat(b);
+}
+console.log(combineArrays(array1, array2));
+console.log(combineArrays([1, 2, 3], [4, 5, 6]));
+*/
